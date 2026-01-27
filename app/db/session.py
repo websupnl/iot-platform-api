b8f40fb5e7450ea452e04a49b5c09385be445225
websupnl <info@websup.nl>
2026-01-27 09:06:24 +0100

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,9 @@
-from sqlalchemy.ext.asyncio import create_async_engine
 import os
+from sqlalchemy.ext.asyncio import create_async_engine
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
