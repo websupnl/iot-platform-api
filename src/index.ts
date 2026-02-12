3924baf6a03a206af7180ad5f7f797a4557c5e80
websupnl <info@websup.nl>
2026-02-12 15:21:58 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { buildServer } from "./server.js";
 
 const app = buildServer();
 
-const port = Number(process.env.PORT ?? 3000);
+const port = Number(process.env.PORT) || 3000;
 
-app.listen({ port, host: "0.0.0.0" }).catch((err) => {
-  app.log.error(err);
-  process.exit(1);
+app.listen({
+  port,
+  host: "0.0.0.0"
 });
